04bce8c6dd96ada40c3edce06516f2278c55b2c1
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2021-10-10 14:26:17 +0200

    Prolazi build.

---
+++ tictactoe.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+using namespace std;
+
+struct tabla
+{
+    int polje[3][3];
+    int unos;
+    bool znak;
+    int pobjednik;
+
+    tabla();
+    void ispisi();
+    bool gameover();
+    bool ponovo();
+    void input();
+    void reset();
+};
+
+tabla::tabla()  //nula je prazno mjesto, 1 je krizic, 2 kruzic
+{
+    for(int i=0; i<3; ++i)
+        for(int j=0; j<3; ++j)
+            polje[i][j]=0;
+    unos=0;
+    znak=1; //krizic pocinje
+    pobjednik=0; //nema pobjednika
+}
+
+void tabla::reset()
+{
+    for(int i=0; i<3; ++i)
+        for(int j=0; j<3; ++j)
+            polje[i][j]=0;
+    unos=0;
+    znak=1;
+    pobjednik=0;
+}
+
+void tabla::ispisi()
+{
+    for(int i=0; i<13; ++i)
+        {
+            if(i%4==0)
+                {
+                    for(int j=0; j<13; ++j)
+                        cout<<"-";
+                }
+
+            else
+                    {
+                        cout<<"|";
+                        for(int k=0; k<3; ++k)
+                            {
+                                cout<<" ";
+                                if(polje[i/4][k] && i%4==2) cout<<polje[i/4][k];
+                                 else cout<<" ";
+                                cout<<" |";
+                            }
+                    }
+            cout<<endl;
+        }
+
+}
+
+bool tabla::gameover()   //checkamo ima li pobjednika i, ako ga nema, jesu li sva polja popunjena
+{
+    for(int i=0; i<3; ++i)
+        {
+            if(polje[i][0]==polje[i][1] && polje[i][1]==polje[i][2] && polje[i][2]!=0)  //retci
+                {pobjednik=polje[i][0]; return true;}
+            if(polje[0][i]==polje[1][i] && polje[1][i]==polje[2][i] && polje[2][i]!=0)  //stupci
+                {pobjednik=polje[0][i]; return true;}
+        }
+    if(polje[0][0]==polje[1][1] && polje[1][1]==polje[2][2] && polje[2][2]!=0)    //dijagonale
+        {pobjednik=polje[0][0]; return true;}
+    if(polje[0][2]==polje[1][1] && polje[1][1]==polje[2][0] && polje[2][0]!=0)
+        {pobjednik=polje[0][2]; return true;}
+
+    //pobjednika nema, jesu li sva polja popunjena?
+    for(int i=0; i<3; ++i)
+        {
+            for(int j=0; j<3; ++j)
+                if (polje[i][j]==0) {return false;}
+        }
+
+    return true;
+}
+
+bool tabla::ponovo()
+{
+    if(pobjednik==0) cout<<"Game over, no winner."<<endl;
+    if(pobjednik==1) cout<<"Game over, X won."<<endl;
+    if(pobjednik==2) cout<<"Game over, O won."<<endl;
+    cout<<"Play again? (0/1) ";
+    bool b; cin>>b;
+    return b;
+}
+
+void tabla::input()  //unosi se dvoznamenkasti broj, prva znamenka je red, druga stupac
+{
+    if(znak==1) cout<<"X turn: "; else cout<<"O turn: ";
+    cin>>unos;
+    while (1)
+        {
+            if(unos/10<1 || unos/10>3 || unos%10<1 || unos%10>3 || unos/100>=1)
+                {
+                    cout<<endl<<"Faulty input. Two-digit number required "<<endl<<"with digits between 1 and 3. Repeat input: ";
+                    cin>>unos;
+                    continue;
+                }
+            if(polje[unos/10][unos%10]!=0)
+                {
+                    cout<<endl<<"Field taken. Choose an empty field: ";
+                    cin>>unos;
+                    continue;
+                }
+            break;
+        }
+    if(znak==1) polje[unos/10-1][unos%10-1]=1;
+     else polje[unos/10-1][unos%10-1]=2;
+
+    return;
+}
+
+int main()
+{
+    cout<<"Tic-tac-toe in command prompt:"<<endl;
+    tabla t;
+    do  {
+            t.reset();
+            while(t.gameover()==false) t.input();
+
+        } while(t.ponovo()==1);
+
+    return 0;
+}
