db8b2734c3a57c74619d94da9a899cbd5c1120b5
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2021-10-16 17:06:12 +0200

    Uljepsana Fibonacci fukcija.
    Umjesto eksplicitne konverzije znakova u stringove, koristim kobinaciju nove Fibonacci() funkcije koja vraca Fibonaccijev broj koji odgovara proslijedjenom znaku i C++ funkcije to_string() unutar <string> libraryja cime izbjegavam motanje sa C-ovskim string funkcijama.

--- konverzija.cpp
+++ konverzija.cpp
@@ -1,38 +1,21 @@
 #include<iostream>
 #include<fstream>
 #include<cctype>
+#include<array>
 using namespace std;
 
-string pretvori(const char c)
+int fibonacci(char c)
 {
-    string s;
-    if(!isalnum(c)) {s+=c; return s;}
-    if (c=='a' || c=='A') {s="0"; return s;}
-    if (c=='b' || c=='B') {s="1"; return s;}
-    if (c=='c' || c=='C') {s="2"; return s;}
-    if (c=='d' || c=='D') {s="3"; return s;}
-    if (c=='e' || c=='E') {s="5"; return s;}
-    if (c=='f' || c=='F') {s="8"; return s;}
-    if (c=='g' || c=='G') {s="13"; return s;}
-    if (c=='h' || c=='H') {s="21"; return s;}
-    if (c=='i' || c=='I') {s="34"; return s;}
-    if (c=='j' || c=='J') {s="55"; return s;}
-    if (c=='k' || c=='K') {s="89"; return s;}
-    if (c=='l' || c=='L') {s="144"; return s;}
-    if (c=='m' || c=='M') {s="233"; return s;}
-    if (c=='n' || c=='N') {s="377"; return s;}
-    if (c=='o' || c=='O') {s="610"; return s;}
-    if (c=='p' || c=='P') {s="987"; return s;}
-    if (c=='q' || c=='Q') {s="1597"; return s;}
-    if (c=='r' || c=='R') {s="2584"; return s;}
-    if (c=='s' || c=='S') {s="4181"; return s;}
-    if (c=='t' || c=='T') {s="6765"; return s;}
-    if (c=='u' || c=='U') {s="10946"; return s;}
-    if (c=='v' || c=='V') {s="17711"; return s;}
-    if (c=='w' || c=='W') {s="28657"; return s;}
-    if (c=='x' || c=='X') {s="46368"; return s;}
-    if (c=='y' || c=='Y') {s="75025"; return s;}
-    if (c=='z' || c=='Z') {s="121393"; return s;}
+    c=tolower(c);
+    static array<int,35> fib;
+    if(fib[1]==1) return fib[c-'a'];
+    int sum=1;
+    for(int i=1; i<35; ++i)
+        {
+            fib[i]=sum;
+            sum=fib[i]+fib[i-1];
+        }
+    return fib[c-'a'];
 }
 
 int main ()
@@ -49,7 +32,8 @@ int main ()
             {
                 for(s=linija.begin(); s!=linija.end(); ++s)
                     {
-                        output<<pretvori(*s);
+                        if(isalpha(*s)) output<<to_string(fibonacci(*s));
+                         else output<<*s;
                     }
                 output<<'\n';
             }
