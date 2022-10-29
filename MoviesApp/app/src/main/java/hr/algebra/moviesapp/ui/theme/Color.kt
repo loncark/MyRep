3c113b00996e4bee9c892004dcce535785ef1376
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2022-10-29 15:58:38 +0200

    Deleted MoviesApp

--- MoviesApp/app/src/main/java/hr/algebra/moviesapp/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package hr.algebra.moviesapp.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val DarkRed = Color(0xFF680303)
-val DarkGreen = Color(0xFF0A4704)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
